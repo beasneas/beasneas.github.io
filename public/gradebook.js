a8aaa474b76fab944f1310dd658500e0a65880d5
beasneas <clairedorsey33@gmail.com>
2025-05-01 16:54:30 -0400

    File management

--- public/gradebook.js
+++ public/gradebook.js
@@ -1,3 +1,5 @@
+// Call the fetchGradeData function to demonstrate the workflow
+fetchGradeData();
 // Fetch data from the PostgreSQL database
 function fetchGradeData() {
     // This function will query the PostgreSQL database and return grade data
@@ -12,13 +14,14 @@ function fetchGradeData() {
         if (xhr.readyState === xhr.DONE) {
             // Check if we're successful
             if (xhr.status !== 200) {
-                console.error(`Could not get grades. Status: ${xhr.status}`);
-                return;
+                console.error(`Could not get grades. 
+                    Status: ${xhr.status}`);
+                
             }
             // And then call the function to update the HTML with our data
             populateGradebook(JSON.parse(xhr.responseText));
         }
-    };
+    }.bind(this);
     xhr.open("GET", apiRoute, true);
     xhr.send();
 }
@@ -53,5 +56,3 @@ function populateGradebook(data) {
     });
 }
 
-// Call the fetchGradeData function to demonstrate the workflow
-fetchGradeData();
