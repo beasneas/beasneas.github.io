ba88d68140d2eb3f8729dcb1b4e00ef51211318e
beasneas <clairedorsey33@gmail.com>
2025-05-01 10:18:25 -0400

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -12,13 +12,14 @@ function fetchGradeData() {
         if (xhr.readyState === xhr.DONE) {
             // Check if we're successful
             if (xhr.status !== 200) {
-                console.error(`Could not get grades. Status: ${xhr.status}`);
-                return;
+                console.error(`Could not get grades. 
+                    Status: ${xhr.status}`);
+                
             }
             // And then call the function to update the HTML with our data
             populateGradebook(JSON.parse(xhr.responseText));
         }
-    };
+    }.bind(this);
     xhr.open("GET", apiRoute, true);
     xhr.send();
 }
@@ -53,5 +54,3 @@ function populateGradebook(data) {
     });
 }
 
-// Call the fetchGradeData function to demonstrate the workflow
-fetchGradeData();
